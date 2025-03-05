ba974aa2d64c8e3f6c8ca70a64a8202b2c440e4b
박소영 <qkrthdud6755@naver.com>
2025-03-05 15:48:22 +0900

    refactor:AppConfig리팩토링 #3

--- src/main/java/spring/basic/AppConfig.java
+++ src/main/java/spring/basic/AppConfig.java
@@ -1,6 +1,8 @@
 package spring.basic;
 
+import spring.basic.discount.DiscountPolicy;
 import spring.basic.discount.FixDiscountPolicy;
+import spring.basic.member.MemberRepository;
 import spring.basic.member.MemberService;
 import spring.basic.member.MemberServiceImpl;
 import spring.basic.member.MemoryMemberRepository;
@@ -12,10 +14,19 @@ import spring.basic.order.OrderServiceImpl;
 //이를 통해 DIP원칙을 준수할 수 있다.
 public class AppConfig {
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    // 어떤 인터페이스에 어떤 구현체를 사용할 것인지 명확해졌다.
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
