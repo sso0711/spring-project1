eeea2095c2dd02da8a7ce67aef9996625bff2086
박소영 <qkrthdud6755@naver.com>
2025-03-26 22:03:22 +0900

    chore: xml기반 스프링 컨테이너 설정 추가 #6

---
+++ src/test/java/spring/basic/xml/XmlAppContext.java
@@ -0,0 +1,16 @@
+package spring.basic.xml;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import spring.basic.member.MemberService;
+
+public class XmlAppContext {
+    @Test
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
